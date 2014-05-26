140f9478c5302287dc5757ba6963d4dced2a25fa
Kathryn Hough <kathrynmhough@gmail.com>
2014-05-26 15:49:29 -0700

    initial commit

---
+++ rails/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Plantitude::Application.config.secret_key_base = '23c72803a3b6cdbea10f65e2b21c165e58e261b4c3fbf559adfdb5ce4acd83c68930906acf00fb110682f5ca1bdbec6e670c2e224416b2af68503aba22b47f29'
